cd85945175e7eb68bb1f7beeb6a122813732a3a6
AK0907HK <aloha.aoto0907@gmail.com>
2026-03-23 10:12:45 +0900

    トップページの編集

--- some-app/app/index.js
+++ some-app/app/index.js
@@ -1,10 +1,4 @@
-import { createRoot } from 'react-dom/client';
-
-function NavigationBar() {
+export function NavigationBar() {
     // TODO: Actually implement a navigation bar
     return <h1>Hello from React!</h1>;
 }
-
-//const domNode = document.getElementById('navigation');
-//const root = createRoot(domNode);
-//root.render(<NavigationBar />);
