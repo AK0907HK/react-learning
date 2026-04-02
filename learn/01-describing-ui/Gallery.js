f8017075cf31d9cf99e53a09dfe5b943594fe454
AK0907HK <aloha.aoto0907@gmail.com>
2026-04-02 09:59:51 +0900

    2jsファイルの追加

---
+++ learn/01-describing-ui/Gallery.js
@@ -0,0 +1,12 @@
+import Profile from "./Profile";
+
+export default function Gallary() {
+    return (
+        <section>
+           <h1>Amazing scientists</h1>
+           <Profile />
+           <Profile />
+           <Profile />
+        </section>
+    );
+}
