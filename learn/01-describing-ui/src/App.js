cf630dfcd004f6cea8bcef317763c90765c956dc
AK0907HK <aloha.aoto0907@gmail.com>
2026-04-08 09:52:16 +0900

    if文でコンポーネントの表示を変更させる

--- learn/01-describing-ui/src/App.js
+++ learn/01-describing-ui/src/App.js
@@ -1,23 +1,38 @@
-import Avator from "./TodoList";
-
-function Card({children}) {
+function Item({ name, isPacked}) {
+    let itemContent = name;
+    if (isPacked) {
+        itemContent= (
+            <del>
+                {name + " ✅"}
+            </del>
+        );
+    }
     return (
-        <div className="card">
-            {children}
-        </div>
+        <li className="item">
+         {itemContent}     
+        </li>
     );
 }
 
-export default function Profile() {
+export default function PackingList() {
+
     return (
-        <Card>
-          <Avator
-            size={100}
-            person={{ 
-            name: 'Katsuko Saruhashi',
-            imageId: 'YfeOqp2'
-            }}                                        
-          />
-        </Card>
+        <section>
+            <h1>Sally Ride's Packing List</h1>
+            <ul>
+                <Item 
+                isPacked={true} 
+                name="Space suit" 
+                />
+                <Item 
+                isPacked={true} 
+                name="Helmet with a golden leaf" 
+                />
+                <Item 
+                isPacked={false} 
+                name="Photo of Tam" 
+                />            
+            </ul>
+        </section>
     );
 }
