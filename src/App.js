ef3cb1ff9748b93f049d1302ef18bd9b74f4ea9a
AK0907HK <aloha.aoto0907@gmail.com>
2026-04-13 10:12:17 +0900

    useStateの実装

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
 // 学習中のチャプターに合わせてimport先を変更してください
 // 例: 02に切り替える場合 → "./learn/02-adding-interactivity/App"
-export { default } from "./learn/01-describing-ui/App";
+export { default } from "./learn/02-adding-interactivity/App";
