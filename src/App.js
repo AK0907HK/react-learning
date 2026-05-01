a002c1b7e64d37216a0cb9e1b0e1d552782eef03
AK0907HK <aloha.aoto0907@gmail.com>
2026-05-01 10:16:09 +0900

    usereduecerの学習

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
 // 学習中のチャプターに合わせてimport先を変更してください
 // 例: 02に切り替える場合 → "./learn/02-adding-interactivity/App"
-export { default } from "./learn/02-adding-interactivity/App";
+//export { default } from "./learn/02-adding-interactivity/App";
+export { default } from "./learn/03-managing-state/App";
+
